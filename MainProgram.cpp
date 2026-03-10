fbd94e36b5beaed3764477a78a7ebaeeb8afae56
sema2817 <semaalmasri06@gmail.com>
2026-03-10 12:40:56 +0300

    Update MainProgram.cpp

--- MainProgram.cpp
+++ MainProgram.cpp
@@ -14,6 +14,7 @@ int addNumbers(int a, int b)
 {
     // TODO
     // Return the sum of a and b
+    return a+b;
 
 }
 
@@ -30,6 +31,11 @@ int sumUpToN(int n)
     // TODO
     // Using a loop calculate the sum
     // 1 + 2 + 3 + ... + n
+    int sum=0;
+    for(int i=1; i<=n; i++){
+        sum +=i;
+    }
+    return sum;
 }
 
 
@@ -40,10 +46,16 @@ PART 3 — Vectors
 ---------------------------------------
 */
 
-int findMaximum(vector<int> numbers)
-{
+int findMaximum(vector<int> numbers){
     // TODO
     // Return the largest number in the vector
+    int maxValue= numbers[0];
+    for(int i=1; i<numbers.size(); i++){
+        if(numbers[i] > maxValue){
+            maxValue = numbers[i];
+        }
+    }
+    return maxValue;
 }
 
 
@@ -58,6 +70,11 @@ string reverseString(string text)
 {
     // TODO
     // Return the reversed version of the string
+    string reversed= "";
+    for(int i=text.length() -1; i>=0; i--){
+        reversed += text[i];
+    }
+    return reversed;
 }
 
 
@@ -79,13 +96,20 @@ public:
     // TODO
     // Create a constructor that receives
     // name and grade
-
+    Student(string n, int g){
+        name=n;
+        grade=g;
+    }
 
     // TODO
     // Create a function printInfo()
     // that prints:
     // Name: <name>
     // Grade: <grade>
+    void printInfo(){
+        cout<< "Name: "<< name << endl;
+        cout<<"Grade: "<< grade << endl;
+    }
 
 };
 
